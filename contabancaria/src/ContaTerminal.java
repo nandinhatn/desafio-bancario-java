8df101ed7e540110b1f5877915dc1f3f103abc7b
nandinhatnl <fernanda@poppymidia.com.br>
2023-05-04 09:41:57 -0300

    adicionando files do projeto bancário funcionando

--- contabancaria/src/ContaTerminal.java
+++ contabancaria/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println ("Por favor, digite o número da Conta");
+        int numero = scanner.nextInt();
+        System.out.println("Por favor, digite sua agência");
+        String agencia = scanner.next();
+        System.out.println("Por favor, digite seu saldo");
+        Double saldo = scanner.nextDouble();
+        System.out.println("Por favor, digite seu nome ");
+        
+        scanner.nextLine();
+        String nome = scanner.nextLine();
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + "conta" + numero + "e seu saldo " + saldo + "ja está disponíveis para saque");
+
+
+
     }
 }
